55fb8c8bbdd9335cf26ce1899114790be7dd93ff
xXHachimanXx <globaldep90@gmail.com>
2021-03-14 09:40:50 -0300

    Creating a class to treat the checkout response.

---
+++ checkout/src/main/java/com/hachiman/ecommercespringstreamskafka/checkout/resource/checkout/CheckoutResponse.java
@@ -0,0 +1,16 @@
+package com.hachiman.ecommercespringstreamskafka.checkout.resource.checkout;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckoutResponse implements Serializable {
+    private String code;
+}
